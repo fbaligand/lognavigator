1e88772112d13bb6a44daec0c0fdc8c2dc4fc1c3
fbaligand <fbaligand@gmail.com>
2014-03-23 23:45:00 +0100

    new twitter-bootstrap 3.1.1 files

--- src/main/webapp/js/lognavigator.js
+++ src/main/webapp/js/lognavigator.js
@@ -1,5 +1,47 @@
 function initPage() {
 	
+	// Enlarge logAccessConfigId combobox if necessary
+	if ($("#logAccessConfigId").width() < 200) {
+		$("#logAccessConfigId").width(200);
+	}
+	
+	// Process logAccessConfigId combobox change
+	$("#logAccessConfigId").change(function(){
+		window.location = "../" + $(this).val() + "/list";
+	});
+	
+	// Encoding buttons
+	var checkedEncodingId = $("input[name=encoding][checked]").attr("id");
+	$("label[for=" + checkedEncodingId + "]").addClass("active");
+	
+	// DisplayType buttons
+	var checkedDisplayTypeId = $("input[name=displayType][checked]").attr("id");
+	$("label[for=" + checkedDisplayTypeId + "]").addClass("active");
+
+	// Auto-Submit command form 
+	$("#commandForm button.btn-primary").click(submitCommandForm);
+	$("#commandForm input:radio").change(submitCommandForm);
+	
+	// Auto-adapt RAW content width
+	if ($("pre").length == 1 && $("pre").width() < $(window).width()) {
+		$("pre").addClass("nofloat");
+	}
+	
+	// Render results table
+	$("#resultsTable").dataTable({
+		bPaginate: false
+//		sPaginationType: "bootstrap",
+//		sDom: "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>"
+	});
+
+}
+
+function submitCommandForm() {
+	$("#commandForm").submit();
+}
+
+function oldInitPage() {
+	
 	// Process logAccessConfigId change
 	$("#logAccessConfigId").change(function(){
 		window.location = "../" + $(this).val() + "/list";
