5ffecbe40ab6f93ed4ee244166b93f9ec8b3eb02
fbaligand <fbaligand@gmail.com>
2015-08-12 11:11:26 +0200

    - make sort working fine when folders are "date patterns" - the "Parent Folder" link is now not sorted and always on top

--- src/main/webapp/js/lognavigator.js
+++ src/main/webapp/js/lognavigator.js
@@ -37,62 +37,78 @@ function initPage() {
 	// Render results table (only if there are less than 1500 lines) 
 	var resultsSize = $("#resultsSize").val();
 	if (resultsSize && resultsSize < 1500) {
-
-		// Render 'size' and 'file' column data
-		var dataTableColumnDefs = [];
-		$("#resultsTable").find("th").each(function(index) {
-			// 'size' column
-			if ($(this).text().match(/size/gi)) {
-				dataTableColumnDefs.push({
-	                render: function ( data, type, row ) {
-	                	if (type == "display" || type == "filter" ) {
-		                    return (data == "-") ? data : numeral(data).format("0.[0] b");
-	                	}
-	                	else {
-	                		return data;
-	                	}
-	                },
-	                targets: index
-	            });
-			}
-			// 'file' column
-			else if ($(this).text().match(/file/gi)) {
-				dataTableColumnDefs.push({
-					render: function ( data, type, row ) {
-						if (type == "sort") {
-							var $data = $(data);
-							var sortedData = $data.hasClass("text-warning") ? "a" : "b";
-							sortedData += $data.text();
-							return sortedData;
-						}
-						else {
-							return data;
-						}
-					},
-					targets: index
-				});
-			}
-		});
-		
-		// Render table using DataTables plugin
-		var datatable = $("#resultsTable").dataTable({
-			bPaginate: false,
-			bStateSave: true,
-			fnStateSaveParams: function (oSettings, oData) {
-				oData.search.search = "";
-			},
-			language: {
-				info: "Showing _TOTAL_ entries",
-				infoEmpty: "Showing 0 entries",
-				sEmptyTable: "No entries to show"
-			},
-			columnDefs: dataTableColumnDefs
-		});
-		
-		// Enable DataTables FixedHeader extension
-		var dataTableOffsetTop = $("#displayTypeTABLE").length > 0 ? 215 : 165;
-		new $.fn.dataTable.FixedHeader(datatable, { offsetTop: dataTableOffsetTop } );
+		renderResultsTable();
 	}
+}
+
+function renderResultsTable() {
+	
+	// Render 'size' and 'file' column data
+	var dataTableColumnDefs = [];
+	$("#resultsTable").find("th").each(function(index) {
+		// 'size' column
+		if ($(this).text().match(/size/gi)) {
+			dataTableColumnDefs.push({
+                render: function ( data, type, row ) {
+                	if (type == "display" || type == "filter" ) {
+	                    return (data == "-") ? data : numeral(data).format("0.[0] b");
+                	}
+                	else {
+                		return data;
+                	}
+                },
+                targets: index
+            });
+		}
+		// 'file' column
+		else if ($(this).text().match(/file/gi)) {
+			dataTableColumnDefs.push({
+				render: function ( data, type, row ) {
+					if (type == "sort") {
+						var $data = $(data);
+						var sortedData = $data.hasClass("text-warning") ? "a" : "b";
+						sortedData += $data.text();
+						return sortedData;
+					}
+					else {
+						return data;
+					}
+				},
+				targets: index,
+				sType: "string"
+			});
+		}
+	});
+	
+	// Don't sort "Parent Folder" line
+	var fnDrawCallback = null;
+	var parentFolderLink = $("#resultsTable a[title='Parent Folder']");
+	if (parentFolderLink.length > 0) {
+		var parentFolderLine = parentFolderLink.closest("tr").remove();
+		fnDrawCallback = function() {
+			$("#resultsTable tbody").prepend(parentFolderLine);
+		}
+	}
+	
+	// Render table using DataTables plugin
+	var datatable = $("#resultsTable").dataTable({
+		bPaginate: false,
+		bStateSave: true,
+		fnStateSaveParams: function (oSettings, oData) {
+			oData.search.search = "";
+		},
+		language: {
+			info: "Showing _TOTAL_ entries",
+			infoEmpty: "Showing 0 entries",
+			sEmptyTable: "No entries to show"
+		},
+		columnDefs: dataTableColumnDefs,
+		fnDrawCallback: fnDrawCallback
+	});
+	
+	// Fix table head line using FixedHeader extension
+	var dataTableOffsetTop = $("#displayTypeTABLE").length > 0 ? 215 : 165;
+	new $.fn.dataTable.FixedHeader(datatable, { offsetTop: dataTableOffsetTop } );
 }
 
 function submitCommandForm() {
