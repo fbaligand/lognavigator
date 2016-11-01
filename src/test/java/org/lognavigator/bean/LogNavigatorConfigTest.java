68ebacaea240e4dddb4faca96ce483e8da107e2b
fbaligand <fbaligand@gmail.com>
2016-11-01 20:27:15 +0100

    fix unit tests running under JDK 1.6

--- src/test/java/org/lognavigator/bean/LogNavigatorConfigTest.java
+++ src/test/java/org/lognavigator/bean/LogNavigatorConfigTest.java
@@ -32,6 +32,9 @@ public class LogNavigatorConfigTest {
 		
 		// Assertions
 		String expectedXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><lognavigator-config><log-access-config id=\"log-access1\" type=\"LOCAL\" user=\"Fabien\" host=\"localhost\" directory=\"/logs\" authorized-users=\"*\" authorized-roles=\"*\" trust=\"false\"/><log-access-config id=\"log-access2\" type=\"LOCAL\" user=\"Fabien\" host=\"localhost\" directory=\"/logs\" authorized-users=\"*\" authorized-roles=\"*\" trust=\"false\"/></lognavigator-config>";
+		if (System.getProperty("java.version").startsWith("1.6.")) {
+			expectedXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><lognavigator-config><log-access-config trust=\"false\" authorized-roles=\"*\" authorized-users=\"*\" directory=\"/logs\" host=\"localhost\" user=\"Fabien\" type=\"LOCAL\" id=\"log-access1\"/><log-access-config trust=\"false\" authorized-roles=\"*\" authorized-users=\"*\" directory=\"/logs\" host=\"localhost\" user=\"Fabien\" type=\"LOCAL\" id=\"log-access2\"/></lognavigator-config>";
+		}
 		assertEquals(expectedXml, xmlOutput.toString());
 	}
 
